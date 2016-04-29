767543feaf697b3a7d65e4b372cd96db7e5f45e4
mwbullen <mwbullen@gmail.com>
2016-04-28 20:15:23 -0400

    Initial Commit

---
+++ Assets/Navigation.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigation : MonoBehaviour {
+
+    
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NavtoGameObject(GameObject target) {
+        NavMeshAgent navAgent = GetComponent<NavMeshAgent>();
+
+        navAgent.SetDestination (target.transform.position);
+
+	}
+
+    public void StopNavigation()
+    {
+        NavMeshAgent navAgent = GetComponent<NavMeshAgent>();
+        navAgent.Stop();        
+    }
+
+    public void ResumeNavigation()
+    {
+        NavMeshAgent navAgent = GetComponent<NavMeshAgent>();
+        navAgent.Resume();
+    }
+
+	public bool HasArrived() {
+		NavMeshAgent navAgent = GetComponent<NavMeshAgent>();
+		if (!navAgent.pathPending && navAgent.remainingDistance <= navAgent.stoppingDistance) {
+			return true;
+		}
+
+		return false;
+	}
+
+
+}
